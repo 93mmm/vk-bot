fcf8bf65efe00b1860edba3f9d697142322004b1
Alexander Reshetnyak <nickolaev513@gmail.com>
2023-08-20 22:54:51 +0300

    get all dialog IDs and names

--- bot/vk_bot.py
+++ bot/vk_bot.py
@@ -13,8 +13,6 @@ from requests import ConnectionError
 
 class Bot:
     def __init__(self):
-        # TODO: program launch config (struct)
-
         self.log = logger.LogsWriter()
         self.json_messages = json_cfg.JsonMessagesHolder()
         self.config = structs.LaunchConfig()
@@ -34,6 +32,9 @@ class Bot:
         except ConnectionError:
             print("Check your internet connection")
             exit()
+
+        if self.config.receive_only_ids:
+            self.get_all_conversations()
 
     def main(self):
         while True:
@@ -63,24 +64,72 @@ class Bot:
     def log_received_message(self, event: Event):
         received_message = structs.ReceivedMessage(event.text,
                                                    event.user_id,
-                                                   self.get_sender_name(event),
+                                                   self.get_sender_name(event.user_id),
                                                    event.peer_id,
-                                                   self.get_conversation_name(event))
+                                                   self.get_conversation_name(event.peer_id))
         self.log.log_received(received_message)
 
-    def get_sender_name(self, event: Event):
-        user = self.api.users.get(user_ids=str(event.user_id))[0]
+    def get_sender_name(self, user_id: int):
+        user = self.api.users.get(user_ids=str(user_id))[0]
         user = f"{user['first_name']} {user['last_name']}"
         return user
 
-    def get_conversation_name(self, event: Event):
-        conv_name = self.api.messages.getConversationsById(peer_ids=event.peer_id,
-                                                           extended=1,
-                                                           fields="chat_settings")
-        if event.from_user:
-            return f"{conv_name['profiles'][0]['first_name']} {conv_name['profiles'][0]['last_name']}"
-        return conv_name["items"][0]["chat_settings"]["title"]
+    def get_conversation_name(self, peer_id: int):
+        response = self.api.messages.getConversationsById(peer_ids=peer_id,
+                                                          extended=1,
+                                                          fields="chat_settings")
+        peer = response["items"][0]["peer"]
+
+        if peer["type"] == "chat":
+            return response["items"][0]["chat_settings"]["title"]
+        elif peer["type"] == "user":
+            profiles = response["profiles"]
+            idx = 0
+            if profiles[1]["id"] == peer_id:
+                idx = 1
+            f"{profiles[idx]['first_name']} {profiles[idx]['last_name']}"
 
     def check_sticker(self, atts):
         if "attach1_type" in atts and atts["attach1_type"] == "sticker":
             self.json_messages.append_sticker(int(atts["attach1"]))
+
+    def get_all_conversations(self):
+        configured_list_of_ids_path = "files/ids/configured_list_of_ids.txt"
+        print("receiving conversations...")
+        offset = 0
+        receive = 200
+        print("\r" + " " * 50, "\r10%, requesting total number of conversations", end="")
+        total_conversations = self.api.messages.getConversations(offset=offset,
+                                                                 count=0,
+                                                                 extended=1,
+                                                                 fields="first_name, last_name")["count"]
+        collected_data = list()
+        collected_data.append("USER ID\t\tNAME OF CHAT\n")
+        print("\r" + " " * 50, "\r30%, requesting data", end="")
+        while total_conversations > offset:
+            conversations = self.api.messages.getConversations(offset=offset,
+                                                               count=receive,
+                                                               extended=1,
+                                                               fields="first_name, last_name")
+            offset += receive
+            users = dict()
+            for el in conversations["profiles"]:
+                users[str(el["id"])] = f"{el['first_name']} {el['last_name']}"
+
+            for el in conversations["items"]:
+                peer = el["conversation"]["peer"]
+                peer_id = str(peer["id"])
+
+                if peer["type"] == "user":
+                    collected_data.append(f"{peer_id}:\t{users[peer_id]}")
+                elif peer["type"] == "chat":
+                    collected_data.append(f"{peer_id}:\t{el['conversation']['chat_settings']['title']}")
+            print("\r" + " " * 50, "\r50%, creating a list with conversations", end="")
+
+            sleep(1)
+
+        with open(configured_list_of_ids_path, "w") as file:
+            print("\r" + " " * 50, "\r70%, creating a file with conversations", end="")
+            file.write("\n".join(collected_data))
+        print("\r" + " " * 50, f"\rCreated file: {configured_list_of_ids_path}")
+        exit()
