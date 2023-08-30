2fbb2cc1da3cbf261a27e9a5ddf3e90cdc075f63
Alexander Reshetnyak <nickolaev513@gmail.com>
2023-08-30 16:48:17 +0300

    improved error tracking

--- structs/events/exceptions.py
+++ structs/events/exceptions.py
@@ -1,18 +1,17 @@
 from datetime import datetime
+from traceback import format_exc
 
 
 class Ex:
-    def __init__(self, ex: Exception, **variables):
-        self.ex: Exception = None
+    def __init__(self, **variables):
         self.variables: dict = None
         self.time: int = None
         
-        self.ex = ex
         self.variables = variables
         self.time = datetime.now()
 
     def __str__(self):
-        message = f"Thrown exception: {repr(self.ex)}\nVariables:\n"
+        message = f"{format_exc()}\nVariables:\n"
         for key, value in self.variables.items():
             message += f"{key} = {value}\n"
         message += f"\nAt the time: {self.time}\n\n"
